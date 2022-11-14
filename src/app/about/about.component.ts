c30f39909d4e4f8905c39492b746d14038d66912
mert.alptekin <mert.alptekin@neomina.com>
2022-11-14 14:27:33 +0300

    1- NgFor Directive 2- NgIf Directive 3- Interface ile Model tanımı 4- ng-template yapısı

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from './User';
 
 @Component({
   selector: 'app-about',
@@ -6,6 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+
+  // *ngIf ve *NgFor directive kullanımı
+
+  // *ngFor ve *ngIf built-in directive ve angularda en çok kullanılan directive olarak karşımıza çıkar.
+
+  users: any[] = [{
+    username: 'ali.test',
+  }, {
+    username: 'mehmet.test'
+  }, {
+    username: 'hülya.test'
+  }]
+
+  users2:User[] = [
+    {
+      username:'cenk',
+      email:'cenk@test.com',
+      website:'http://cenk.test.com'
+    },
+    {
+      username:'ahmet',
+      email:'ahmet@test.com'
+    }
+  ]
+
+
+
+
 
   constructor() { }
 
