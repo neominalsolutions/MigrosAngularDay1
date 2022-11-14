5d4c7b34d40ba5f44d751a2a268122a45ee82686
mert.alptekin <mert.alptekin@neomina.com>
2022-11-14 14:04:47 +0300

    1-Property Binding Örneği 2-Event Binding Örneği

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  // default da angularda tanımlanan değişkeler public olarak kendi htmlden çağırıabilir.
+  private title:string = 'home';
+  content:string = 'Home Page';
+  counter:number = 0;
+
   constructor() { }
 
   ngOnInit() {
+  
+  }
+
+  onClick(){
+    alert('Clicked');
+  }
+
+  onCountUp(){
+    this.counter++;
+    console.log('count-up', this.counter);
+  }
+
+  onCountDown(){
+    this.counter--;
+    console.log('count-down', this.counter);
   }
 
 }
